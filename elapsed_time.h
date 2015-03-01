ab9ce0b37933983761243efaaf25c2e85a5e3437
Hawken Rives <hawkrives@gmail.com>
2015-03-01 10:36:22 -0600

    More of the same

--- elapsed_time.h
+++ elapsed_time.h
@@ -9,26 +9,23 @@ using std::endl;
 u_int64_t start_time;  // might have to use uint64_t
 
 inline
-u_int64_t read_TSC()
-{
-  u_int64_t i;
-  __asm__ __volatile__ ("rdtsc" : "=&A" (i));
-  return i;
+u_int64_t read_TSC() {
+	u_int64_t i;
+	__asm__ __volatile__ ("rdtsc" : "=&A" (i));
+	return i;
 }
 
 inline
-void start_timer()
-{
-  start_time = read_TSC();
+void start_timer() {
+	start_time = read_TSC();
 }
 
 inline
-double elapsed_time()
-{
-  u_int64_t curr_time = read_TSC();
-  double elapsed = curr_time - start_time;
-//  if ( elapsed < 0 ) cerr << "Warning:  negative number of cycles in elapsed_time!" << endl;
-  return elapsed;
+double elapsed_time() {
+	u_int64_t curr_time = read_TSC();
+	double elapsed = curr_time - start_time;
+	// if ( elapsed < 0 ) cerr << "Warning:  negative number of cycles in elapsed_time!" << endl;
+	return elapsed;
 }
 
 #endif
