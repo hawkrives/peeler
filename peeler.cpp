5e31960f069e0c8f793471155b7bb697143bf1e1
Hawken Rives <hawkrives@gmail.com>
2015-03-01 21:00:19 -0600

    rearrange functions

--- peeler.cpp
+++ peeler.cpp
@@ -6,82 +6,6 @@
 using namespace std;
 #include "elapsed_time.h"
 
-struct Dictionary {
-	vector<string> wordArray;   // store the data
-	unordered_map<int, vector<string> > hashTable;
-
-	Dictionary(const char *filename);  // constructor
-
-	bool inWordArray(string &s);       // single query
-	void check(const char *filename);  // multiple queries
-};
-
-void getWords(const char *filename, vector<string> &vec, unordered_map<int, vector<string> > &map) {
-	ifstream f(filename);
-	if ( ! f.good() ) {
-		cerr << "Error:  unable to open " << filename << endl;
-		exit(-1);
-	}
-	string s;
-	while ( f >> s ) {
-		vec.push_back(s);
-		int hash = hash_string(s);
-		try {
-			vector<string> *current = map.at(hash);
-			current.push_back(s);
-		} catch (int e) {
-			vector<string> strings = {s};
-			map.insert(make_pair(hash, strings));
-		}
-	}
-}
-
-Dictionary::Dictionary( const char *filename ) {
-	getWords(filename, wordArray, hashTable);
-}
-
-bool Dictionary::inWordArray(string &s) {
-	int hash = hash_string(*s);
-
-	try {
-		vector<string> possibilities = hashTable.at(hash);
-		for (string str : possibilities)
-			if (str == *s)
-				return true;
-	} catch (int err) {}
-
-	return false;
-
-	// int n = wordArray.size();
-	// for ( int i = 0; i < n; ++i ) {
-	// 	if ( s == wordArray[i] ) {
-	// 		return true;
-	// 	}
-	// }
-	//
-	// return false;
-}
-
-void Dictionary::check( const char *filename ) {
-	vector<string> query;
-	unordered_map<int, vector<string> > queryHash;
-	getWords(filename, query, queryHash);
-
-	start_timer();  // from elapsed_time.h
-
-	int counter = 0, n = query.size();
-	for ( int i = 0; i < n; ++i ) {
-		if ( !inWordArray(query[i]) ) {
-			// cout << query[i] << " ";
-			++counter;
-		}
-	}
-	cout << "Misspelled " << counter << " words." << endl;
-
-	double cycles = elapsed_time();
-	cout << " Total cycles: " << cycles << endl;
-}
-
 vector<string> quadrize(string input) {
 	size_t len = input.size();
 	size_t quadtrant = len / 4;
@@ -156,13 +80,89 @@ size_t hash_string(string s) {
   return hash_vector(strs_to_ascii(quadrize(s)));
 }
 
-int main(int argc, char **argv) {
-	// if ( argc != 3 ) {
-	// 	cerr << "Usage: spellCheck dictionaryFile inputFile" << endl;
-	// 	exit(0);
+struct Dictionary {
+	vector<string> wordArray;   // store the data
+	unordered_map<int, vector<string> > hashTable;
+
+	Dictionary(const char *filename);  // constructor
+
+	bool inWordArray(string &s);       // single query
+	void check(const char *filename);  // multiple queries
+};
+
+void getWords(const char *filename, vector<string> &vec, unordered_map<int, vector<string> > &map) {
+	ifstream f(filename);
+	if ( ! f.good() ) {
+		cerr << "Error:  unable to open " << filename << endl;
+		exit(-1);
+	}
+	string s;
+	while ( f >> s ) {
+		vec.push_back(s);
+		int hash = hash_string(s);
+		try {
+			vector<string> &current = map.at(hash);
+			current.push_back(s);
+		} catch (...) {
+			vector<string> strings = {s};
+			map.insert(make_pair(hash, strings));
+		}
+	}
+}
+
+Dictionary::Dictionary( const char *filename ) {
+	getWords(filename, wordArray, hashTable);
+}
+
+bool Dictionary::inWordArray(string &s) {
+	int hash = hash_string(s);
+
+	try {
+		vector<string> possibilities = hashTable.at(hash);
+		for (string str : possibilities)
+			if (str == s)
+				return true;
+	} catch (...) {}
+
+	return false;
+
+	// int n = wordArray.size();
+	// for ( int i = 0; i < n; ++i ) {
+	// 	if ( s == wordArray[i] ) {
+	// 		return true;
+	// 	}
 	// }
-	// Dictionary d(argv[1]);
-	// d.check(argv[2]);
+	//
+	// return false;
+}
+
+void Dictionary::check( const char *filename ) {
+	vector<string> query;
+	unordered_map<int, vector<string> > queryHash;
+	getWords(filename, query, queryHash);
+
+	start_timer();  // from elapsed_time.h
+
+	int counter = 0, n = query.size();
+	for ( int i = 0; i < n; ++i ) {
+		if ( !inWordArray(query[i]) ) {
+			// cout << query[i] << " ";
+			++counter;
+		}
+	}
+	cout << "Misspelled " << counter << " words." << endl;
+
+	double cycles = elapsed_time();
+	cout << " Total cycles: " << cycles << endl;
+}
+
+int main(int argc, char **argv) {
+	if ( argc != 3 ) {
+		cerr << "Usage: spellCheck dictionaryFile inputFile" << endl;
+		exit(0);
+	}
+	Dictionary d(argv[1]);
+	d.check(argv[2]);
 
 	vector<string> test = quadrize("pneumonoultramicroscopicsilicovolcanoconiosis");
 	cout << test << endl;
