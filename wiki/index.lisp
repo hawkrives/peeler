4643ffd11d87f92be6826d35cb5d0d58bd07f509
Hawken Rives <hawkrives@gmail.com>
2015-03-01 23:22:51 -0600

    Added wiki pages

---
+++ wiki/index.lisp
@@ -0,0 +1,8 @@
+(h1 "peeler")
+
+(p "Matthew Johnson and Hawken Rives' speel checker, for Algorithms (Spring '15).")
+
+(ul
+	(li (a 'href "Peeler/buckets" "Spread of words in buckets"))
+	(li "Query efficiency:" (a 'href "Peeler/exist" "if the word exists") ", and " (a 'href "Peeler/no_exist" "if the word does not exist."))
+	(li "Code!" (a 'href "Peeler/cpp" "file") (a 'href "http://stogit.cs.stolaf.edu/rives/peeler" "git repo")))
