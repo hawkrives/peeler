dec53eb4733cf9676126fc982045a40438c79855
Hawken Rives <hawkrives@gmail.com>
2015-03-03 20:22:05 -0600

    Add get_timer function

--- elapsed_time.h
+++ elapsed_time.h
@@ -8,23 +8,25 @@ using std::endl;
 
 u_int64_t start_time;  // might have to use uint64_t
 
-inline
-u_int64_t read_TSC() {
+inline u_int64_t read_TSC() {
 	u_int64_t i;
 	__asm__ __volatile__ ("rdtsc" : "=&A" (i));
 	return i;
 }
 
-inline
-void start_timer() {
+inline void start_timer() {
 	start_time = read_TSC();
 }
 
-inline
-double elapsed_time() {
+inline u_int64_t get_timer() {
+	return read_TSC();
+}
+
+inline double elapsed_time() {
 	u_int64_t curr_time = read_TSC();
 	double elapsed = curr_time - start_time;
-	// if ( elapsed < 0 ) cerr << "Warning:  negative number of cycles in elapsed_time!" << endl;
+	if ( elapsed < 0 )
+		cerr << "Warning: negative number of cycles in elapsed_time!" << endl;
 	return elapsed;
 }
 
